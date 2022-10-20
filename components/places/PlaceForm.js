79d07dd00381744cbe46aaf5805cc33aea75c829
unknown <majdi.mokhtar@gmail.com>
2022-10-20 01:06:49 +0100

    adding image picker component

--- components/places/PlaceForm.js
+++ components/places/PlaceForm.js
@@ -1,42 +1,47 @@
-import React, { useState } from 'react';
-import {View, StyleSheet, Text, ScrollView, TextInput} from 'react-native';
-import {Colors} from "../../constants/colors"
+import React, { useState } from "react"
+import { View, StyleSheet, Text, ScrollView, TextInput } from "react-native"
+import { Colors } from "../../constants/colors"
+import ImagePicker from "./ImagePicker"
 
 const PlaceForm = () => {
-    const [entredeTitle,setEntredTitle] =useState("")
-    function changeTitelHandler(entredText){
-        setEntredTitle(entredText)
-    }
-    return (
-        <ScrollView  style={styles.form} >
-            <View>
-            <Text style={styles.label}>The Place Form</Text>
-            <TextInput onChangeText={changeTitelHandler} value={entredeTitle} style={styles.input} />
-            </View>
-        </ScrollView>
-    );
+  const [entredeTitle, setEntredTitle] = useState("")
+  function changeTitelHandler(entredText) {
+    setEntredTitle(entredText)
+  }
+  return (
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>The Place Form</Text>
+        <TextInput
+          onChangeText={changeTitelHandler}
+          value={entredeTitle}
+          style={styles.input}
+        />
+      </View>
+      <ImagePicker />
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
-    form :{
-        flex : 1,
-        padding : 24
-    },
-    label :{
-        fontWeight : "bold",
-        marginBottom : 4,
-        color : Colors.primary500
-    },
-    input :{
-        marginVertical : 8,
-        paddingHorizontal : 4,
-        paddingVertical : 8,
-        fontSize : 16,
-        borderBottomColor : Colors.primary700,
-        borderBottomWidth : 2,
-        backgroundColor : Colors.primary100
-    }
-
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
 })
 
-export default PlaceForm;
+export default PlaceForm
